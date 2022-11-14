14e9b6b6cc728135f071deb064b615d4a5689d05
Abigail Douglas <EburgAbby@aol.com>
2022-11-13 23:53:21 -0800

    addeed scripts to app.js need to fix imgs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-// TODO: Add a comment explaining what this import statement is doing
-import HelloReact from './components/HelloReact';
+import Home from './pages/Home';
+
+
+
+
+// import React from 'react';
+// // TODO: Add a comment explaining what this import statement is doing
+// import HelloReact from './components/HelloReact';
 
 // TODO: Add a comment explaining the purpose of the App component
 function App() {
-  return <HelloReact />;
+  
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
+
 }
 
 export default App;
